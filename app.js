85e960f6e25a5c78d3b39d9370bf822f45e1df46
Keyen331 <banayos.kay@gmail.com>
2016-11-08 17:47:58 -1000

    updating

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Create all the things
 var hero = "Superman";
 var gender = "male";
 var age = 23;
@@ -26,9 +27,11 @@ if (age <= 18) {
   xp = "has lived through many epic battles";
 }
 
+//Superman is our lovable hero, who has seen a few battles with his hammer.
+
 // 1 fish 2 fish
 var count = undefined;
-var animal = "Scary Shark"
+var animal = "Shark"
 function animalHouse(p1, p2) {
   return count = p1 + p2;
 }
@@ -36,15 +39,43 @@ function animalHouse(p1, p2) {
 //1 Fish 2 Many Fish (cont.)
 if (count > 2) {
   console.log('We have to many elephants')
-}else (count = false) {
-  console.log('Welcome to the club')
+}else if (count = false) {
+  console.log('Welcome to the club');
 }
 
 //Assemble the Avengers
-var hero = "Hard Ironman";
-function avengersAssemble(Thor) {
-  if (true) {}
+var hero = "Hulk";
+function avengersAssemble(Avengers) {
+  if (Avengers = "Thor") {
+    console.log(Thor + "magnet");
+  }else if (Avengers = "ironman") {
+    console.log("Sorry, you are not needed for this mission");
+  }else if (Avengers = "captain america") {
+    console.log("Sorry, you are not needed for this mission");
+  }else if (Avengers = "black widow") {
+    console.log("Sorry, you are not needed for this mission");
+  }
+  return hero = Avengers;
 }
 
 //Lights out
-var lights = 
+var lights = On;
+if (lights = On) {
+  console.log("And then there was light");
+}else (Lights = false) {
+  console.log("Lights out");
+}
+
+//Vroom Vroom
+var carType = "Toyota";
+var color = "black";
+var num = 31;
+if (carType = "Toyota" && color = "blue") {
+  console.log("we have" + carType + color);
+}else if (carType = Lamborghini && color = "red") {
+  console.log("We have" + carType + color);
+}else if (carType = Tesla && color = black) {
+  console.log("We have" + carType + color);
+}else {
+  console.log("We don't have any blue Toyotas in stock");
+}
